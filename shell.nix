0c21b55927a433505b9ff3d008ccce4b7fb333ad
Raymond Gauthier <jraygauthier@gmail.com>
2015-11-07 14:58:29 -0500

    Initial working build.

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, bytestring, directory, gitit, hslogger
+      , mtl, network, network-uri, stdenv, utf8-string
+      }:
+      mkDerivation {
+        pname = "jrg-gitit";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base bytestring directory gitit hslogger mtl network network-uri
+          utf8-string
+        ];
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
